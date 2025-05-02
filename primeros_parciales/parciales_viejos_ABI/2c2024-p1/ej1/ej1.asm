fb72d98fc7c63e34bb2f2e969ff3fad17a35da25
Lisandro Cordoba <lisandrocordoba11@gmail.com>
2025-05-01 22:33:52 -0300

    pase con todos los tests (abi+valgrind) el 2c-2024(P1)

--- primeros_parciales/parciales_viejos_ABI/2c2024-p1/ej1/ej1.asm
+++ primeros_parciales/parciales_viejos_ABI/2c2024-p1/ej1/ej1.asm
@@ -14,21 +14,21 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - es_indice_ordenado
 global EJERCICIO_1A_HECHO
-EJERCICIO_1A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - indice_a_inventario
 global EJERCICIO_1B_HECHO
-EJERCICIO_1B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ITEM_NOMBRE EQU ??
-ITEM_FUERZA EQU ??
-ITEM_DURABILIDAD EQU ??
-ITEM_SIZE EQU ??
+ITEM_NOMBRE EQU 0
+ITEM_FUERZA EQU 20
+ITEM_DURABILIDAD EQU 24
+ITEM_SIZE EQU 28
 
 ;; La funcion debe verificar si una vista del inventario está correctamente 
 ;; ordenada de acuerdo a un criterio (comparador)
@@ -59,11 +59,70 @@ es_indice_ordenado:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = item_t**     inventario
-	; r/m64 = uint16_t*    indice
-	; r/m16 = uint16_t     tamanio
-	; r/m64 = comparador_t comparador
-		ret
+	; rdi = item_t**     inventario
+	; rsi = uint16_t*    indice
+	; dx = uint16_t     tamanio
+	; rcx = comparador_t comparador
+
+	; prologo
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	sub rsp, 8	; PILA ALINEADA
+
+	; preservo en no volatiles
+	xor rbx, rbx
+	mov bx, dx		; rbx = tamanio
+	mov r12, rdi	; r12 = *inventario
+	mov r13, rsi	; r13 = *indice
+	mov r14, rcx	; r14 = comparador
+
+	dec rbx 		; rbx = i = (tamanio-1)
+	.loop:
+		; busco el item actual
+		xor r8, r8
+		mov r8w, word [r13 + rbx*2]			; r8 = indice_item_actual = indice[i];
+		mov r9, qword [r12 + r8*8]			; r9 = item_actual = inventario[indice_item_actual];
+
+		; busco el item anterior
+		xor r10, r10
+		mov r10w, word [r13 + (rbx-1)*2]	; r10 = indice_item_anterior = indice[i-1];
+		mov r11, qword [r12 + r10*8]		; r11 = item_anterior = inventario[indice_item_anterior];
+
+		; los comparo
+		mov rdi, r11
+		mov rsi, r9
+		call r14
+		; si estan desordenados return false
+		cmp rax, 0
+		je .estan_desordenados
+		; sino sigo
+		.siguiente_iteracion:
+		dec rbx
+		jnz .loop 
+	
+	; si termine y es indice ordenado
+	mov rax, 1
+	jmp .fin
+
+	.estan_desordenados:
+	; rax ya es 0, no tengo q hacer nada
+
+	.fin:
+	; epilogo
+	add rsp, 8
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
+
+	ret
 
 ;; Dado un inventario y una vista, crear un nuevo inventario que mantenga el
 ;; orden descrito por la misma.
@@ -91,7 +150,50 @@ indice_a_inventario:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = item_t**  inventario
-	; r/m64 = uint16_t* indice
-	; r/m16 = uint16_t  tamanio
+	; rdi = item_t**  inventario
+	; rsi = uint16_t* indice
+	; dx = uint16_t  tamanio
+
+	; prologo
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+	push r13
+	push r14	; PILA ALINEADA	
+
+	; preservo en no volatiles
+	xor rbx, rbx
+	mov bx, dx		; rbx = tamanio
+	mov r12, rdi	; r12 = *inventario
+	mov r13, rsi	; r13 = *indice
+
+	; reservo memoria para inventario_reordenado
+	mov rdi, rbx	
+	sal rdi, 3		; rdi = tamaño*sizeof(*item_t)
+	call malloc
+	mov r14, rax	; r14 = *inventario_reordenado
+
+	dec rbx 		; rbx = i = (tamanio-1)
+	.loop:
+		; busco el item actual
+		xor r8, r8
+		mov r8w, word [r13 + rbx*2]			; r8 = indice_item_actual = indice[i];
+		mov r9, qword [r12 + r8*8]			; r9 = item_actual = inventario[indice_item_actual];
+
+		mov qword [r14 + rbx*8], r9			; inventario_reordenado[i] = item_actual
+
+		;siguiente_iteracion:
+		dec rbx
+		cmp rbx, -1		; Pues quiero entrar al while cuando i = 0
+		jg .loop 
+
+	mov rax, r14
+	; epilogo
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
+
 	ret
--- primeros_parciales/parciales_viejos_ABI/2c2024-p1/ej1/ej1.c
+++ primeros_parciales/parciales_viejos_ABI/2c2024-p1/ej1/ej1.c
@@ -24,6 +24,20 @@ bool EJERCICIO_1B_HECHO = false;
  * OPCIONAL: implementar en C
  */
 bool es_indice_ordenado(item_t** inventario, uint16_t* indice, uint16_t tamanio, comparador_t comparador) {
+	uint16_t indice_item_actual, indice_item_anterior;
+	item_t *item_actual,*item_anterior = NULL;
+	bool ordenado = false;
+
+	for(int i = (tamanio-1); i > 0; i--){
+		indice_item_actual = indice[i];
+		item_actual = inventario[indice_item_actual];
+
+		indice_item_anterior = indice[i-1];
+		item_anterior = inventario[indice_item_anterior];
+
+		ordenado = comparador(item_anterior, item_actual);
+		if (ordenado == false) return false;		
+	}
 	return true;
 }
 
@@ -32,6 +46,9 @@ bool es_indice_ordenado(item_t** inventario, uint16_t* indice, uint16_t tamanio,
  */
 item_t** indice_a_inventario(item_t** inventario, uint16_t* indice, uint16_t tamanio) {
 	// ¿Cuánta memoria hay que pedir para el resultado?
-	item_t** resultado;
+	item_t** resultado = malloc(tamanio*8);	// sizeof(item_t* = 8)
+	for(int i = tamanio-1; i >= 0; i--){
+		resultado[i] = inventario[indice[i]];
+	}
 	return resultado;
 }
